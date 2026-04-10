39db19520d7c6a96e8bba224d4ad22b2f47f2051
Daniel Wells <112579766+dan424gg@users.noreply.github.com>
2026-04-09 21:45:20 -0700

    reduce boilerplate code

--- flutter/lib/routes/app_router.dart
+++ flutter/lib/routes/app_router.dart
@@ -5,6 +5,38 @@ import '../theme/app_theme.dart';
 
 @AutoRouterConfig() // tells AutoRoute this is the central navigation config
 class AppRouter extends $AppRouter {
+  // Helper method to reduce boilerplate for slide transitions
+  static Widget _buildSlideTransition({
+    required Offset slideDirection,
+    required Animation<double> animation,
+    required Animation<double> secondaryAnimation,
+    required Widget child,
+  }) {
+    final curvedAnimation = CurvedAnimation(
+      parent: animation,
+      curve: AppAnimations.curve,
+    );
+    final curvedSecondaryAnimation = CurvedAnimation(
+      parent: secondaryAnimation,
+      curve: AppAnimations.curve,
+    );
+
+    final slideInAnimation = Tween<Offset>(
+      begin: slideDirection,
+      end: Offset.zero,
+    ).animate(curvedAnimation);
+
+    final slideOutAnimation = Tween<Offset>(
+      begin: Offset.zero,
+      end: slideDirection,
+    ).animate(curvedSecondaryAnimation);
+
+    return SlideTransition(
+      position: slideOutAnimation,
+      child: SlideTransition(position: slideInAnimation, child: child),
+    );
+  }
+
   // list of routes to be used
   @override
   List<AutoRoute> get routes => [
@@ -15,31 +47,11 @@ class AppRouter extends $AppRouter {
       durationInMilliseconds: AppAnimations.duration.inMilliseconds,
       reverseDurationInMilliseconds: AppAnimations.duration.inMilliseconds,
       transitionsBuilder: (context, animation, secondaryAnimation, child) {
-        // Apply curve to both animations
-        final curvedAnimation = CurvedAnimation(
-          parent: animation,
-          curve: AppAnimations.curve,
-        );
-        final curvedSecondaryAnimation = CurvedAnimation(
-          parent: secondaryAnimation,
-          curve: AppAnimations.curve,
-        );
-
-        // When entering: slide in from left
-        final slideInAnimation = Tween<Offset>(
-          begin: const Offset(-1.0, 0.0),
-          end: Offset.zero,
-        ).animate(curvedAnimation);
-
-        // When exiting (pushed by another route): slide out to left
-        final slideOutAnimation = Tween<Offset>(
-          begin: Offset.zero,
-          end: const Offset(-1.0, 0.0),
-        ).animate(curvedSecondaryAnimation);
-
-        return SlideTransition(
-          position: slideOutAnimation,
-          child: SlideTransition(position: slideInAnimation, child: child),
+        return _buildSlideTransition(
+          slideDirection: const Offset(-1.0, 0.0),
+          animation: animation,
+          secondaryAnimation: secondaryAnimation,
+          child: child,
         );
       },
     ),
@@ -49,31 +61,11 @@ class AppRouter extends $AppRouter {
       durationInMilliseconds: AppAnimations.duration.inMilliseconds,
       reverseDurationInMilliseconds: AppAnimations.duration.inMilliseconds,
       transitionsBuilder: (context, animation, secondaryAnimation, child) {
-        // Apply curve to both animations
-        final curvedAnimation = CurvedAnimation(
-          parent: animation,
-          curve: AppAnimations.curve,
-        );
-        final curvedSecondaryAnimation = CurvedAnimation(
-          parent: secondaryAnimation,
-          curve: AppAnimations.curve,
-        );
-
-        // When entering: slide in from right
-        final slideInAnimation = Tween<Offset>(
-          begin: const Offset(1.0, 0.0),
-          end: Offset.zero,
-        ).animate(curvedAnimation);
-
-        // When exiting (pushed by another route): slide out to right
-        final slideOutAnimation = Tween<Offset>(
-          begin: Offset.zero,
-          end: const Offset(1.0, 0.0),
-        ).animate(curvedSecondaryAnimation);
-
-        return SlideTransition(
-          position: slideOutAnimation,
-          child: SlideTransition(position: slideInAnimation, child: child),
+        return _buildSlideTransition(
+          slideDirection: const Offset(1.0, 0.0),
+          animation: animation,
+          secondaryAnimation: secondaryAnimation,
+          child: child,
         );
       },
     ),
